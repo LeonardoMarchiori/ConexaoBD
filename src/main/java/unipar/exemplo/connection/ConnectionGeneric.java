29079789aafc05bbb0846ae086842f1dda1bbc24
LeonardoMarchiori <leonardo.marchiori01@gmail.com>
2023-06-07 19:57:17 -0300

    Initial commit

---
+++ src/main/java/unipar/exemplo/connection/ConnectionGeneric.java
@@ -0,0 +1,10 @@
+package unipar.exemplo.connection;
+
+public class Connection {
+
+    private final String url = "jdbc:postgresql://localhost:5432/";
+    private final String username = "";
+    private final String password = "";
+    private final String dbName = "";
+
+}
